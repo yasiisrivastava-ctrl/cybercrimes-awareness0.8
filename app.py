a5e715f0d0da8db253089352aeeaeb3d2568d3e0
My name is Yashi, i am here to build things for fun. <yasiisrivastava@gmail.com>
2026-07-05 22:25:31 +0000

    Added safety section and improved UI

--- app.py
+++ app.py
@@ -6,10 +6,23 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/info')
 def info():
     return render_template('info.html')
 
+@app.route('/report')
+def report():
+    return render_template("report.html")
+
+@app.route("/safety")
+def safety():
+    return render_template("safety.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-     
+
+
+
+
+        
